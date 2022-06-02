eae2ff224e739651b935a14f2c788bbfad7b824a
AngelahMbula <mbula.angelah@gmail.com>
2022-06-02 03:38:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main void function takes no arguments, entry point
+ * returns 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
