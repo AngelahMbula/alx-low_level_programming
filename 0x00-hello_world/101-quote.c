825069dc39cc0ea9d1ea62b6d34da110d824b80a
AngelahMbula <mbula.angelah@gmail.com>
2022-06-02 05:23:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - takes no arguments
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 80);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 80);
 	return (1);
 }
