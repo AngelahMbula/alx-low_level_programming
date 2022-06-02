39dccdac756de6b9e652cc915cd8cce78d31d131
AngelahMbula <mbula.angelah@gmail.com>
2022-06-02 05:15:24 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - takes no arguments
+ * write - prints the words
+ * Return: 1 value always
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 80);
+	return (1);
+}
