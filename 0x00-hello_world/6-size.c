a3cb54e34b5622b4a8c252f15be77a21b3860458
AngelahMbula <mbula.angelah@gmail.com>
2022-06-02 04:33:26 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int (LL);
 	float (F);
 
-	printf("Size of char: %c\n", sizeof(C));
+	printf("Size of char: %d\n", sizeof(C));
 	printf("Size of int: %i\n", sizeof(I));
 	printf("Size of long int: %li\n", sizeof(L));
-	printf("Size of long long int: %lli\n", sizeof(LL));
-	printf("Size of float: %f\n", sizeof(F));
+	printf("Size of long long int: %d\n", sizeof(LL));
+	printf("Size of float: %d\n", sizeof(F));
 
 	return (0);
 }
