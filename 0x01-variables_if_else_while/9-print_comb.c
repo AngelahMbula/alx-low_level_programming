a9de0572289e24db3c02ae8bfcc12fda617651ae
AngelahMbula <mbula.angelah@gmail.com>
2022-06-05 10:23:28 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *  main - Prints combinations of single digit numbers
+ *  Return: 0 value always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i==)
+	putchar((i/10) + '0');
+	putchar((i % 10) + '0');
+	putchar(',');
+	putchar(' ');
+	return (0);
+}
