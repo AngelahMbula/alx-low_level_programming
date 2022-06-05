1e557ef33862779f9e6d234d31e4791bb43d4fbe
AngelahMbula <mbula.angelah@gmail.com>
2022-06-05 06:39:16 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point takes no arguments
+ * Return: 0 value always
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch= 'a'; ch<= 'z'; ch++)
+		putchar(ch);
+		putchar('\n');
+
+	return (0);
+}
