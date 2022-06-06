13cbdc2f6885aad2dacaa4f499b28496dc0feb37
AngelahMbula <mbula.angelah@gmail.com>
2022-06-06 01:58:17 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,14 +17,14 @@ int main(void)
 	{
 	for (k = j + 1; k <= 9; k++)
 	{
-	putchar(i + '0');
-	putchar(j + '0');
-	putchar(k + '0');
 	if (i != j && j != k)
 	{
 	putchar(',');
 	putchar(' ');
 	}
+	putchar(i + '0');
+	putchar(j + '0');
+	putchar(k + '0');
 	}
 	}
 	}
