f075c9c447db4bb56401700eb6c64afca6fffcf9
AngelahMbula <mbula.angelah@gmail.com>
2022-06-06 06:04:20 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,11 +12,11 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-	for (j = 0; j <= 9; j++)
+	for (j = i + 1; j <= 9; j++)
 	{
 	putchar(i + '0');
 	putchar(j + '0');
-	if (!(i == 9 && j == 9))
+	if (i < 8)
 	{
 	putchar(',');
 	putchar(' ');
