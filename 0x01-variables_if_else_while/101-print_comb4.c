d3768cabc5ead4bbec2c0b9e02f8550880705760
AngelahMbula <mbula.angelah@gmail.com>
2022-06-06 01:11:18 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,11 +20,14 @@ int main(void)
 	putchar(i + '0');
 	putchar(j + '0');
 	putchar(k + '0');
-	}
-	}
-	}
+	if (i != 9 && j != 9 && k != 9)
+	{
 	putchar(',');
 	putchar(' ');
+	}
+	}
+	}
+	}
 	putchar('\n');
 	return (0);
 }
