1760a527c891b81af783df7d3bf477f588d866cf
AngelahMbula <mbula.angelah@gmail.com>
2022-06-06 00:53:39 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *  main - Prints all possible combinations, three digits
+ *  Return: 0 value always
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = i+1; j <= 9; j++)
+	{
+	for (k = j+1; k <= 9; k++)
+	{
+	putchar(i + '0');
+	putchar(j + '0');
+	putchar(k + '0');
+	}
+	}
+	}
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+}
