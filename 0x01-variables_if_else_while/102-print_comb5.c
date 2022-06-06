5077c79b8978487f268ce244e6f2384d5a442523
AngelahMbula <mbula.angelah@gmail.com>
2022-06-06 02:27:08 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *  main - Prints combinations, two two digits
+ *  Return: 0 value always
+ */
+int main(void)
+{
+	int n;
+	int i;
+
+	for (n = 0; n < 99; n++)
+	for (i = n;;)
+	{
+	if ((i /= 10) == 0)
+	{
+	putchar(n);
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+	}
+	}
+	return (0);
+}
