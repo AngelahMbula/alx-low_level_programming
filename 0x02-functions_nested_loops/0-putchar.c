34ccbd2726fe227a0e914caafcf46f444ec8aa06
AngelahMbula <mbula.angelah@gmail.com>
2022-06-07 03:20:13 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * main - Prints the word "_putchar"
+ * Return: 0 value always
+ */
+int main(void)
+{
+	int i;
+	char s[] = "_putchar";
+
+	for (i = 0; i < s; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
