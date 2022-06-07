0c8182140620d4a3ffa140812b3bb25ea5a39b9c
AngelahMbula <mbula.angelah@gmail.com>
2022-06-07 05:32:46 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,9 @@
  * main - Prints the word "_putchar"
  * Return: 0 value
  */
-int _putchar(char c)
-
 int main(void)
 {
-	int c = _putchar;
+	char c = _putchar;
 
 	return (write(1, &c, 1));
 	return (0);
