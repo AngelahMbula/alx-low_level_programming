bf958d547b0c606c64ce869d709ecec0285579d8
AngelahMbula <mbula.angelah@gmail.com>
2022-06-08 02:26:58 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h.
+
+/**
+ * main - entry point
+ * Return: 0 value always
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,8 +1,21 @@
 #include "main.h"
 #include <unistd.h>
-
+#include <stdio.h>
 
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+
+void print_alphabet_x10(void);
+{
+	int i = 0;
+	char letter;
+
+	while (i++ <= 9)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+	}
 }
