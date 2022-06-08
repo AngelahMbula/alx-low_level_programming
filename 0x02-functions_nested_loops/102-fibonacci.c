fd394f285ee0e102ab5fb6ff7125572726d51bf7
AngelahMbula <mbula.angelah@gmail.com>
2022-06-08 11:15:01 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 value always
+ */
+int main(void)
+{
+	int f1 = 1, f2 = 2, next, i;
+
+	for (i = 0; i < 50; i++)
+	{
+		if (i < 1)
+			next = c;
+		else
+		{
+			next = f1 + f2;
+			f1 = f2;
+			f2 = f1;
+		}
+		printf("%d, ", next);
+	}
+	printf("\n";
+	return (0);
+}
