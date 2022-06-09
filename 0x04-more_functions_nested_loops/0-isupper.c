5feecf4ef28dcdbb18cf87db867810c5b3734b4e
AngelahMbula <mbula.angelah@gmail.com>
2022-06-09 09:42:06 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,20 @@
 #include "main.h"
-#include <unistd.h>
+#include <stdio.h>
+
 /**
  * _isupper - checks for uppercase characters
  * @c: character to be chacked
  * Return: 1 if successful, 0 otherwise 
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-	return (1);
+		return (1);
 	}
 	else
 	{
-	return (0);
+		return (0);
 	}
 }
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -14,9 +14,8 @@
  * On error, -1 is returned, and errno is set appropriately.
 
  */
+
 int _putchar(char c)
 {
-
 	return (write(1, &c, 1));
-
 }
