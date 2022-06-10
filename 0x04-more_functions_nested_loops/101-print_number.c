5a0aab1253b8b4d33f1d259ec6175bafcdb63558
AngelahMbula <mbula.angelah@gmail.com>
2022-06-10 07:39:16 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,15 +10,17 @@
 
 void print_number(int n)
 {
+	unsigned int num = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -num;
 	}
-	if (n == 0)
+	if (num == 0)
 		_putchar('0');
-	if (n / 10)
-		printnumber(n / 10);
+	if (num / 10)
+		printnumber(num / 10);
 
-	_putchar((n % 10) + '0');
+	_putchar((num % 10) + '0');
 }
