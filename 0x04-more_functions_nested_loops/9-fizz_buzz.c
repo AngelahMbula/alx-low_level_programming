3e1a3521fa8f986f61220ba144665a44282036ef
AngelahMbula <mbula.angelah@gmail.com>
2022-06-10 08:12:49 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,19 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 && i % 5 == 0 )
+	if (i % 3 && i % 5 == 0 )
 		{
 			printf("FizzBuzz ");
 		}
-		else if (i % 3 == 0)
+	else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (i % 5 == 0);
+	else if (i % 5 == 0);
 		{
 			printf("Buzz ");
 		}
-		else
+	else
 		{
 			printf("%d ", i);
 		}
