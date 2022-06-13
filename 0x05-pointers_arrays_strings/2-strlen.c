9fff389df9a31400f5d5b901fe582f1d3d9047e6
AngelahMbula <mbula.angelah@gmail.com>
2022-06-13 09:22:19 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: value to be evaluated
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	return c;
+}
