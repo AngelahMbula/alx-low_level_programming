8106808c73d53615c7c5ad1b7081726b84c3190b
AngelahMbula <mbula.angelah@gmail.com>
2022-06-20 08:13:47 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,9 +14,9 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < rows; i++)
 	{
-		for (j = 0; j <= 8; j++)
+		for (j = 0; j < 8; j++)
 		{
-			printf("%d ", a[i][j]);
+			printf("%d", a[i][j]);
 			if (j == 8)
 			{
 				printf("\n");
