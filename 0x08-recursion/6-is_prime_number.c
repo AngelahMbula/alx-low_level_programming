68743e7c212771678fabe537a0684e698abebe51
AngelahMbula <mbula.angelah@gmail.com>
2022-06-22 09:27:20 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ * @n: given input
+ * Return: 0 if number is divisible
+ * 1 if number is not divisible
+ */
+
+int is_prime_number(int n)
+{
+	int num;
+
+	if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		if (num % n == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (is_prime_number(num, n - 1);
+		}
+	}
+}
