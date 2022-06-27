c2ea3bcbae76b3d6f8bd612e7e502ace63c2b2dc
AngelahMbula <mbula.angelah@gmail.com>
2022-06-27 05:15:34 -0700

    2--str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,13 +12,13 @@ char *str_concat(char *s1, char *s2)
 {
 	char *concat_str;
 
-	int i = 0, j = 0;
+	int i = 0, j = 0, len = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1[i] || s2[j]; j++;)
+	for (i = 0; s1[i] || s2[j]; j++)
 		len++;
 	concat_str = malloc(sizeof(char) * (i + j + 1 + 1));
 	if (concat_str == NULL)
