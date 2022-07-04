44f21eeb35a6e0d3b7245000a3b2e3085e33fb3f
AngelahMbula <mbula.angelah@gmail.com>
2022-07-04 07:45:00 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H_
 #define DOG_H_
-
+/**
+ * struct dog - define a new struct dog with name, age, owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 struct dog
 {
 	char *name;
