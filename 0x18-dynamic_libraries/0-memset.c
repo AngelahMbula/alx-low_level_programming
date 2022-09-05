eb6da028d6a249faa3d61410d9dee410e9e770ad
AngelahMbula <mbula.angelah@gmail.com>
2022-09-05 03:56:03 -0700

    libdynamic.so

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+#include <string.h>
+
+
+
+/**
+ *
+ *  * _memset - fills memory with constant byte
+ *
+ *   * @s:
+ *
+ *    * @b:
+ *
+ *     * @n:
+ *
+ *      * Return: pointer to memory area s
+ *
+ *       */
+
+
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+
+		int i = 0;
+
+
+
+			while (n > 0)
+
+					{
+
+								s[i] = b;
+
+										i++;
+
+												n--;
+
+													}
+
+				return (s);
+
+}
