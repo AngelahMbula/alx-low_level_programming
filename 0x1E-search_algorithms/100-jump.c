c1e32d93b89c170f25d442ad54e4c2607e76265a
AngelahMbula <mbula.angelah@gmail.com>
2023-01-05 04:47:25 -0800

    initial commit

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,9 @@
+#include "search_algos.h"
+/**
+ * jump_search - function searches for a value in array of integers
+ * @array: pointer to first element of array
+ * @size: number of elements in array
+ * @value: value to be searched for
+ * Return: first index where value is located
+ */
+int jump_search(int *array, size_t size, int value)
